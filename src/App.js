225efdf6f28cafa0260de7bc86e1056687b7e4c9
Nikolay <34944975+seb510@users.noreply.github.com>
2023-05-07 14:13:58 +0300

    add modal/animation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -12,6 +14,7 @@ function App() {
     ])
 
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const[modal, setModal] = useState(false)
 
     const sortedPosts = useMemo(()=>{
         if(filter.sort){
@@ -26,6 +29,7 @@ function App() {
 
     const createPost = (newPost)=> {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -34,17 +38,13 @@ function App() {
 
   return (
     <div className="App">
-        <PostForm create={createPost}/>
+        <MyButton onClick={()=>{setModal(true)}}>Создать пост</MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
         <hr style={{margin  : '15px 0'}}/>
         <PostFilter filter={filter} setFilter={setFilter}/>
-        {sortedAndSearchedPosts.length
-            ?
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
-            :
-            <h2 style={{textAlign: 'center'}}>
-                Посты не найдены
-            </h2>
-        }
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
 
     </div>
   );
